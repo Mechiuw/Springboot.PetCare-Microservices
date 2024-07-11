e3b755117ea0aa7ec6d86367d8754a180783c967
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-11 18:37:48 +0700

    [FEATURE] Added Shelter Response

---
+++ src/main/java/com/mcsoftware/petcare/model/dto/response/ShelterResponse.java
@@ -0,0 +1,26 @@
+package com.mcsoftware.petcare.model.dto.response;
+
+import com.mcsoftware.petcare.constant.EStatus;
+import com.mcsoftware.petcare.model.entity.Pet;
+import com.mcsoftware.petcare.model.entity.VaccinatePoint;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ShelterResponse {
+    private String name;
+    private String address;
+    private String email;
+    private String city;
+    private String postalCode;
+    private EStatus isActive;
+    private List<Pet> petInShelter;
+    private List<VaccinatePoint> vaccinatePoints;
+}
