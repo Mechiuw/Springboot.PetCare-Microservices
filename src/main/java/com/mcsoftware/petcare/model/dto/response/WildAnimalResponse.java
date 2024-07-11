cc2deabaeb7f96f7bff55cb9a513c0a4bb0a22c3
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-11 18:43:44 +0700

    [FEATURE] Added Wild Animal Response

---
+++ src/main/java/com/mcsoftware/petcare/model/dto/response/WildAnimalResponse.java
@@ -0,0 +1,20 @@
+package com.mcsoftware.petcare.model.dto.response;
+
+import com.mcsoftware.petcare.constant.EAnimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class WildAnimalResponse {
+    private String breed;
+    private EAnimal animalType;
+    private String medicalConditions;
+    private String locationFound;
+    private Boolean isAlive;
+    private String vaccinatePointId;
+}
