3482ec10437fb08d3e68a88cc930a7d71b9492ec
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-12 12:31:44 +0700

    [WIP] Wip service provider service

--- src/main/java/com/mcsoftware/petcare/service/interfaces/ShelterService.java
+++ src/main/java/com/mcsoftware/petcare/service/interfaces/ShelterService.java
@@ -9,14 +9,20 @@ import com.mcsoftware.petcare.model.entity.VaccinatePoint;
 import java.util.List;
 
 public interface ShelterService {
+    //CRUD
     ShelterResponse create(ShelterRequest shelterRequest);
     ShelterResponse update(String id, ShelterRequest shelterRequest);
     void delete(String id);
     ShelterResponse getById(String id);
     List<Shelter> getAll();
+
+    //BUSINESS SERVICE
     List<Pet> getAllPetInShelter(String id);
     List<VaccinatePoint> getVaccinatePointList(String id);
 
+    //HELPER / FINDER
     Shelter shelterFinder(String id);
+
+    //VALIDATOR
     Shelter shelterValidator(Shelter shelter);
 }
