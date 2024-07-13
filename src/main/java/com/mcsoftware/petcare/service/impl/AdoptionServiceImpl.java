4fc3c47a08aabde8f98ea4e4f278c0d7781fbe8e
Matthew Diamonda <144647347+Mechiuw@users.noreply.github.com>
2024-07-13 21:13:14 +0700

    [FEAUTRE] Added clientfinder

--- src/main/java/com/mcsoftware/petcare/service/impl/AdoptionServiceImpl.java
+++ src/main/java/com/mcsoftware/petcare/service/impl/AdoptionServiceImpl.java
@@ -6,6 +6,7 @@ import com.mcsoftware.petcare.model.entity.Adoption;
 import com.mcsoftware.petcare.repository.AdoptionRepository;
 import com.mcsoftware.petcare.service.interfaces.AdoptionService;
 import com.mcsoftware.petcare.utils.converter.TransactionBuilderConverter;
+import com.mcsoftware.petcare.utils.finder.TransactionFinder;
 import com.mcsoftware.petcare.utils.validator.TransactionValidator;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
@@ -20,15 +21,16 @@ import java.util.List;
 @Transactional(rollbackOn = Exception.class)
 public class AdoptionServiceImpl implements AdoptionService {
     private final AdoptionRepository adoptionRepository;
-    private final TransactionBuilderConverter transactionConverter;
+    private final TransactionBuilderConverter converter;
     private final TransactionValidator validator;
+    private final TransactionFinder finder;
     @Override
     public AdoptionResponse create(AdoptionRequest adoptionRequest) throws IllegalAccessException {
         try {
-            Adoption newAdoption = transactionConverter.adoptionRequestToAdoption(adoptionRequest);
+            Adoption newAdoption = converter.adoptionRequestToAdoption(adoptionRequest);
             Adoption validatedAdoption = validator.adoptionValidator(newAdoption);
             Adoption savedAdoption = adoptionRepository.save(validatedAdoption);
-            return transactionConverter.adoptionToAdoptionResponse(savedAdoption);
+            return converter.adoptionToAdoptionResponse(savedAdoption);
         } catch (ValidationException e){
             throw new RuntimeException("Validation error: " + e.getMessage());
         } catch (EntityNotFoundException e){
@@ -41,7 +43,9 @@ public class AdoptionServiceImpl implements AdoptionService {
     @Override
     public AdoptionResponse update(String id, AdoptionRequest adoptionRequest) {
         try {
-            return null
+            Adoption findAdoption = finder.adoptionFinder(id);
+            findAdoption.setClientId();
+            return null;
         } catch (ValidationException e){
             throw new RuntimeException("Validation error: " + e.getMessage());
         } catch (EntityNotFoundException e){
--- src/main/java/com/mcsoftware/petcare/utils/finder/TransactionFinder.java
+++ src/main/java/com/mcsoftware/petcare/utils/finder/TransactionFinder.java
@@ -1,7 +1,9 @@
 package com.mcsoftware.petcare.utils.finder;
 
 import com.mcsoftware.petcare.model.entity.Adoption;
+import com.mcsoftware.petcare.model.entity.Client;
 import com.mcsoftware.petcare.repository.AdoptionRepository;
+import com.mcsoftware.petcare.repository.ClientRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,11 +12,21 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class TransactionFinder {
     private final AdoptionRepository adoptionRepository;
+    private final ClientRepository clientRepository;
 
     public Adoption adoptionFinder(String id){
         try{
             return adoptionRepository.findById(id)
                     .orElseThrow(() -> new EntityNotFoundException("not found any adoption with id: " + id));
+        } catch (Exception e){
+            throw new RuntimeException(e.getCause());
+        }
+    }
+
+    public Client clientFinder(String id){
+        try{
+            return clientRepository.findById(id)
+                    .orElseThrow(() -> new EntityNotFoundException("not found any client with id: " + id));
         } catch (Exception e){
             throw new RuntimeException(e.getCause());
         }
